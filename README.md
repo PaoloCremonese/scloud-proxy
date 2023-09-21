6b932c0b5bb421d903b5845afd254778a2522114
Paolo Cremonese <paolo.a.cremonese@gmail.com>
2023-09-21 16:40:34 +0200

    21/09/2023 16:40

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # scloud-proxy
+
+Scloud with proxy structure for Dockered deploy
+
+# Configurazione
+1. va aggiunta la chiave privata teledata.website.key in WildflyContainerSrc_thirdtry
+2. Nel FileSystem della macchina ospite si deve creare una cartella
+        /home/teledata/scloud-proxy/TeleCLOUD/scloud/nginx/certificati
+   dove vanno copiati i certificati letsencrypt:
+        fullchain.pem
+        privkey.pem
---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Build SCLOUD script
+
+echo "Build SCLOUD containers"
+
+# 1.  stop containers
+echo "Stop containers"
+cd /home/teledata/scloud-proxy
+docker compose down
+
+# 2.  supposing all the files has been placed in the right place, build the container
+echo "Build Wildfly"
+cd /home/teledata/scloud-proxy/WildflyContainerSrc_thirdtry
+docker build paolocremonese/wf-9.0.2.final:thirdtry .
+
+# 3.  start the container again
+echo "Start containers again"
+cd /home/teledata/scloud-proxy
+docker compose up --detach
+
+echo "DONE !!!"
+
+
+
