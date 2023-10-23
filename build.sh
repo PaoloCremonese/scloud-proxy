86b58159cf8f022210152e2c76a160acb8e3b96a
Paolo Cremonese <paolo.a.cremonese@gmail.com>
2023-10-23 17:57:29 +0200

    23-10-2023 17:57

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ docker compose down
 # 2.  supposing all the files has been placed in the right place, build the container
 echo "Build Wildfly"
 cd /home/teledata/scloud-proxy/WildflyContainerSrc_thirdtry
-docker build paolocremonese/wf-9.0.2.final:thirdtry .
+docker build --tag paolocremonese/wf-9.0.2.final:thirdtry .
 
 # 3.  start the container again
 echo "Start containers again"
